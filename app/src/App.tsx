548dcbe0c58106249585a0bceedcb80d6bf1d6a3
Sam Kitchen <samkitchen94@gmail.com>
2022-07-19 16:12:59 +0100

    first commit

---
+++ app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './styles/main.scss';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Wordle Clone</h1>
+    </div>
+  );
+}
+
+export default App;
